91d612a00fca6abf5e25f5f437fc45c96cce5acf
Sava Bezanovic <sava.bezanovic@quantox.com>
2020-02-20 12:59:46 +0100

    Database

--- simpleorm.sql
+++ simpleorm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Feb 14, 2020 at 09:54 AM
+-- Generation Time: Feb 20, 2020 at 11:59 AM
 -- Server version: 8.0.19
 -- PHP Version: 7.4.1
 
@@ -25,20 +25,66 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `pets`
+--
+
+CREATE TABLE `pets` (
+  `id` int NOT NULL,
+  `name` varchar(48) NOT NULL,
+  `breed` varchar(48) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `user_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `pets`
+--
+
+INSERT INTO `pets` (`id`, `name`, `breed`, `user_id`) VALUES
+(1, 'Lessie', 'doggo', 75),
+(2, 'Bea', 'Collie', 75),
+(24, 'Lessie', 'Collie', 75),
+(25, 'Lessie', 'Collie', 75),
+(26, 'Lessie', 'Collie', 75),
+(27, 'Lessie', 'Collie', 75);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` int NOT NULL,
-  `firstName` varchar(48) NOT NULL,
-  `lastName` varchar(48) NOT NULL,
+  `first_name` varchar(48) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `last_name` varchar(48) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `age` int NOT NULL,
-  `pet` varchar(48) NOT NULL
+  `pet_type` varchar(48) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `age`, `pet_type`) VALUES
+(75, 'TRUC', 'Dogg', 48, NULL),
+(1095, 'BLA', 'Bezanovic', 23, 'doggo'),
+(1159, 'Sava', 'Bezanovic', 23, 'doggo'),
+(1160, 'Spasoje', 'Spasic', 30, NULL),
+(1161, 'Sava', 'Bezanovic', 23, 'doggo'),
+(1162, 'Spasoje', 'Spasic', 30, NULL),
+(1182, 'Spasoje', 'Spasic', 30, NULL),
+(1183, 'Sava', 'Bezanovic', 23, 'doggo');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `pets`
+--
+ALTER TABLE `pets`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -51,10 +97,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `pets`
+--
+ALTER TABLE `pets`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1199;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pets`
+--
+ALTER TABLE `pets`
+  ADD CONSTRAINT `pets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
